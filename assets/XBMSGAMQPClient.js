a9dc7b79ae3248eed635ddfa4029e97b60d61fe7
Antonio Maradiaga <antonio.maradiaga@sap.com>
2024-07-12 15:58:09 +0200

    Renaming subscriptions

--- assets/XBMSGAMQPClient.js
+++ assets/XBMSGAMQPClient.js
@@ -2,10 +2,13 @@ const assert = require("assert");
 const AMQP = require("@sap/xb-msg-amqp-v100");
 
 module.exports = class AMQPClient {
+
   /**
    * CAP Nodejs using the AMQP protocol. This class is a wrapper around the @sap/xb-msg-amqp-v100 library.
+   * @param {String} clientName Name of the client. Used for logging purposes.
+   * @param {Object} credentials Connection details for the AMQP broker. This should be specified in cds.requires.AMQP.credentials
    */
-  constructor(clientName, credentials, useJSON = true) {
+  constructor(clientName, credentials) {
     this.clientName = clientName;
     this.connectionInfo = credentials;
     this.broker = null;
@@ -16,7 +19,7 @@ module.exports = class AMQPClient {
   }
 
   /**
-   * Connects the client to the cloud broker. Connection details should be specified in cds.requires.AMQP.credentials
+   * Connects the client to the event broker. Connection details should be specified in cds.requires.AMQP.credentials
    */
   connect = () => {
     assert(this.broker == null, "Broker is already connected.");
@@ -41,21 +44,21 @@ module.exports = class AMQPClient {
   /**
    * Creates an Broker receiver which will receive events on the specified topics.
    * @param {Service} service CDS Service that should receive events on the topics. This name should include the namespace.
-   * @param {String[]} topics Name (or array of names) of topics that should be received.
+   * @param {String[]} subscriptions Name (or array of names) of topics or queues that should be received.
    */
-  registerSubscriber = (service, topics = []) => {
+  registerSubscriber = (service, subscriptions = []) => {
     assert(this.broker, "Broker is not connected.");
-    (Array.isArray(topics) ? topics : [topics]).forEach((topic) => {
-      const receiver = this.broker.receiver(topic).attach(topic);
-      console.log(`${this.clientName} > Receiver created for ${topic}`);
+    (Array.isArray(subscriptions) ? subscriptions : [subscriptions]).forEach((subscription) => {
+      const receiver = this.broker.receiver(subscription).attach(subscription);
+      console.log(`${this.clientName} > Receiver created for ${subscription}`);
 
       receiver.on("error", (err) => {
         return new Error(err);
       });
       receiver.on("data", (msg) => {
-        console.log(`${this.clientName} > Received on topic ${topic}`);
+        console.log(`${this.clientName} > Received on topic ${subscription}`);
         const data = msg.payload.data || msg.payload.toString("utf-8");
-        service.emit(topic, JSON.parse(data));
+        service.emit(subscription, JSON.parse(data));
         return msg.done();
       });
     });
