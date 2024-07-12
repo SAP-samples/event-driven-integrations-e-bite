78fae0812a8977bce5e703716cea69ff3a95ad98
Antonio Maradiaga <antonio.maradiaga@sap.com>
2024-07-12 15:57:31 +0200

    Renaming client

--- 04-emis/cap/coffee-roaster-consumer/srv/server.js
+++ 04-emis/cap/coffee-roaster-consumer/srv/server.js
@@ -2,9 +2,9 @@ const cds = require('@sap/cds');
 const AMQPClient = require('../../../../assets/XBMSGAMQPClient');     // --> uses package @sap/xb-msg-amqp-v100
 
 cds.on('served', async (services) => {
-    const EMISClient = new AMQPClient("EMIS", cds.env.requires.AMQP.credentials);
-    EMISClient.connect().then(() => {
-        EMISClient.registerSubscriber(services['ebite.ConsumerService'], 'queue:queue-CAP');
+    const client = new AMQPClient(cds.env.requires.AMQP.brokerName, cds.env.requires.AMQP.credentials);
+    client.connect().then(() => {
+        client.registerSubscriber(services['ebite.ConsumerService'], ['queue://queue-CAP']);
     });
 });
 module.exports = cds.server;
--- 04-emis/cap/coffee-roaster-consumer/srv/service.js
+++ 04-emis/cap/coffee-roaster-consumer/srv/service.js
@@ -1,8 +1,13 @@
 const cds = require('@sap/cds');
-const queue = 'queue:queue-CAP';
+const queue = 'queue://queue-CAP';
+const tryMeTopic = 'topic://try-me';
 
 module.exports = async (srv) => {
     srv.on(queue, async req => {
         console.log('received >', req.event, req.data);
     });
+
+    // srv.on(tryMeTopic, async req => {
+    //     console.log('received >', req.event, req.data);
+    // });
 }
