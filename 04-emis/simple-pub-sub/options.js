8f7c8bed8926fe089e25ca32441acc12840f04f2
Antonio Maradiaga <antonio.maradiaga@sap.com>
2024-08-02 17:01:25 +0200

    Updating options with placeholders

--- 04-emis/simple-pub-sub/options.js
+++ 04-emis/simple-pub-sub/options.js
@@ -1,13 +1,13 @@
 const options = {
     'oa2': {
         'client':
-            'sb-DB02C2AB-5847-4CB1-BDEC-789B76ECE326-a36046f8-a4b5-483c-bc9e-17885b1fef64!b369290|eventing-xsuaa-broker!b486577',
+            'sb-CC22W8JN-4516-1GS3-ESEH-.....!b68542|eventing-xsuaa-broker!b62872',
         'secret':
-            '9645a557-47bd-48d5-9355-fc00e4c0febe$mpGjrQY6ksLFrX0Lr2KiQONYtqFadpadQSXydF6uYhU=',
+            '85cub18w-or63-1854-f0n3-r6.....Xob0=',
         'endpoint':
-            'https://emea-south-integrationsuite-m4e7rsns.authentication.eu10.hana.ondemand.com/oauth/token'
+            'https://<subdomain>.authentication.<region>.hana.ondemand.com/oauth/token'
     },
-    'uri': 'wss://emea-south-integra-bd650b76-ebb1-4bd7-99ca-09fcd3fa09ed.eu10.a.eventmesh.integration.cloud.sap/protocols/amqp10ws'
+    'uri': 'wss://<emis-instance>.<region>.a.eventmesh.integration.cloud.sap/protocols/amqp10ws'
 };
 
 module.exports = options;
